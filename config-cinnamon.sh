947fe40dc0eb19875babeec5102444635ae75222
joao <jr8116@gmail.com>
2019-02-25 10:54:03 +0000

    Organizing manjaro startup as a branch

--- config-cinnamon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PARENT_FOLDER=~/Code
-DOTFILES_FOLDER=$PARENT_FOLDER/dotfiles
-
-dconf load /org/cinnamon/ < $DOTFILES_FOLDER/dconf/org.cinnamon
-dconf load /com/gexperts/Tilix/ < $DOTFILES_FOLDER/dconf/com.gexperts.Tilix
-pkill -HUP -f "cinnamon --replace"
--- start-manjaro.sh
+++ start-manjaro.sh
@@ -2,6 +2,7 @@
 
 PARENT_FOLDER=~/Code
 DOTFILES_FOLDER=$PARENT_FOLDER/dotfiles
+CONFIG_FOLDER=~/.config
 
 # optimizing and updating pacman
 pacman-optimize && sync
@@ -11,6 +12,10 @@ pacman -Syu
 sudo pacman -S snapd
 sudo systemctl enable snapd
 sudo systemctl start snapd
+
+# themes and fonts
+pacman -S otf-fira-code
+pacman -S arc-gtk-theme
 
 # emacs
 pacman -Sy emacs
@@ -28,8 +33,11 @@ sudo systemctl start snapd
 sudo pacman -S zsh
 sudo pacman -S the_silver_search
 sudo pacman -S firefox
-sudo snap install telegram-desktop spotify gravit-designer
+sudo snap install telegram-desktop spotify gravit-designer mattermost-desktop
 sudo snap install --classic slack
+
+# go
+sudo pacman -S go
 
 # Defining ZSH as default shell
 chsh -s /bin/zsh
@@ -43,14 +51,43 @@ mv ~/.zshrc ~/.zshrc.bak
 ln -s $DOTFILES_FOLDER/zsh/.zshrc ~/.zshrc
 ln -s $DOTFILES_FOLDER/zsh/.zshenv ~/.zshenv
 
+# nvmshenv
+
 # nvm
-pacman -S nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nv
 
 # pyenv
-pacman -S pyenv
-pacman -S pyenv-virtualenvwrapper
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+sudo pacman -S pyenv-virtualenvwrapper
 zsh -c 'pyenv install 3.6.8 && pyuenv global system 3.6.8'
 
 # global node modules
 zsh -c 'nvm install stable && nvm use stable \
 && npm install -g flow flow-bin typescript tern eslint prettier babel-eslint eslint-plugin-react js-beautify eslint-plugin-mocha eslint-plugin-flowtype eslint-plugin-jasmine eslint-plugin-jsx-control-statements eslint-plugin-promise eslint-plugin-jest eslint-plugin-import eslint-plugin-prettier eslint-config-prettier'
+
+#i3wm
+
+# install i3blocks
+git clone https://github.com/vivien/i3blocks $PARENT_FOLDER/i3blocks
+cd $PARENT_FOLDER/i3blocks
+./autogen.sh
+./configure
+make
+make install
+
+# blocks contrib
+git clone https://github.com/vivien/i3blocks-contrib ~/.local/src/i3blocks-contrib
+
+
+# Linking configs
+mv $CONFIG_FOLDER/i3/config $CONFIG_FOLDER/i3/config.bkp
+mv $CONFIG_FOLDER/i3blocks/ $CONFIG_FOLDER/i3blocks.bkp
+mv $CONFIG_FOLDER/rofi/ $CONFIG_FOLDER/rofi.bkp
+mv $CONFIG_FOLDER/dunst/ $CONFIG_FOLDER/dunst.bkp
+
+ln -s $DOTFILES_FOLDER/i3/manjaro-config $CONFIG_FOLDER/i3/config
+ln -s $DOTFILES_FOLDER/i3blocks/ $CONFIG_FOLDER/i3blocks
+ln -s $DOTFILES_FOLDER/rofi/ $CONFIG_FOLDER/rofi
+ln -s $DOTFILES_FOLDER/dunst/ $CONFIG_FOLDER/dunst
