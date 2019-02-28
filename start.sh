cad53b5cc24fb5d7aa2a7c4bb0bea60ff744098b
joao <joao.ferreira@corp.badoo.com>
2019-02-28 15:12:32 +0000

    Pulse as daemon

--- start.sh
+++ start.sh
@@ -139,6 +139,7 @@ ln -s $DOTFILES_FOLDER/i3blocks/ $CONFIG_FOLDER/i3blocks
 ln -s $DOTFILES_FOLDER/rofi/ $CONFIG_FOLDER/rofi
 ln -s $DOTFILES_FOLDER/dunst/ $CONFIG_FOLDER/dunst
 ln -s $DOTFILES_FOLDER/terminator/ $CONFIG_FOLDER/terminator
+ln -s $DOTFILES_FOLDER/pulse/client.conf $CONFIG_FOLDER/pulse/client.conf
 
 # sudo cp $DOTFILES_FOLDER/compton/.Backlight /usr/share/X11/xorg.conf.d/20-intel.conf
 
