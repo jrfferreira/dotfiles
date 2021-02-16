ecdc3364da7aa6917f8b03b429e426cd1ba3916d
Joao <joao.ferreira@>
2021-02-16 15:03:45 +0000

    small tweak on start

--- start.sh
+++ start.sh
@@ -21,7 +21,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 
     confirm_and_install "snapd" ./scripts/snapd.sh
 
-    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git
+    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev git autoconf
     
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     set +e
